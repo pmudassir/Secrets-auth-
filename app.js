8066d349ea7b08eefa0fd8cbb53fd788d0a8b800
pmudassir <pmuddasir@gmail.com>
2022-12-13 10:57:53 +0530

    passport.js with  session & cookie

--- app.js
+++ app.js
@@ -4,14 +4,27 @@ const express = require("express");
 const ejs = require ("ejs");
 const bodyParser = require ("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session')
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+//passport and session configs
+
+app.use(session({
+    secret: "Our little secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.authenticate('session'));
+app.use(passport.initialize());
+app.use(passport.session());
  
 mongoose.set('strictQuery', true);
 mongoose.set('strictQuery', false);
@@ -22,52 +35,84 @@ const userSchema = new mongoose.Schema({
     password: String 
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function (req, res){
-    res.render("home");
+    if(req.isAuthenticated()){
+        res.redirect("/secrets")
+    } else{
+        res.render("home");
+    }
 });
 
 app.get("/login", function(req,res){
-    res.render("login");
+    if(req.isAuthenticated()){
+        res.redirect("/secrets")
+    } else{
+        res.render("login");
+    }
+    
 });
 
 app.get("/register", function(req, res){
-    res.render("register");
+    if(req.isAuthenticated()){
+        res.redirect("/secrets")
+    } else{
+        res.render("register");
+    }
 });
 
-app.post("/register", function(req, res){
+app.get("/secrets", function(req, res){
+    //below code kills the session avoids the back button issue
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stal   e=0, post-check=0, pre-check=0');
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        })
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            } else{
-                res.render("secrets")
-            }
-        })
-    });
+    if (req.isAuthenticated()) {
+        res.render("secrets")
+    } else {
+        res.redirect("/login")
+    }
 })
 
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
+app.get("/logout", function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect("/");
+    });
+  });
 
-    User.findOne({email: username}, function(err, foundUser){
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if (err) {
+            console.log(err);
+            res.redirect("/register")
+        } else {
+            passport.authenticate("local")(req, res ,function(){
+                res.redirect("/secrets")
+            })
+        }
+    })
+})
+
+app.post("/login", passport.authenticate("local"), function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.logIn(user, function(err){
         if(err){
             console.log(err);
         } else {
-            if (foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result === true) {
-                        res.render("secrets")
-                    }
-                });
-            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            })
         }
     })
 })
